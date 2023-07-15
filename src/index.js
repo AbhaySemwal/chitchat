e16a39e35c51f22cda37f8345b11e9733d0894f4
AbhaySemwal <shbhtsemwal@gmail.com>
2023-07-16 01:04:56 +0530

    minor_fixes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
+import "./global.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
